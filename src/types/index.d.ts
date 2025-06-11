e0c166a2bb605013fb57f724b6bedde5b77bfb7c
Volker Mauel <volkermauel@gmail.com>
2025-06-11 10:08:55 +0200

    :bug: Fix: add openweb-ui platform support

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -2,6 +2,13 @@ interface IStringKeyMap {
   [propName: string]: any
 }
 
-type supportedPlatforms = 'official' | 'azure' | 'gemini' | 'ollama' | 'groq' | 'openweb'
+type supportedPlatforms =
+  | 'official'
+  | 'azure'
+  | 'gemini'
+  | 'ollama'
+  | 'groq'
+  | 'openweb'
+  | 'openweb-ui'
 
-type insertTypes = 'replace' |  'append' |  'newLine' | 'NoAction'
+type insertTypes = 'replace' | 'append' | 'newLine' | 'NoAction'
